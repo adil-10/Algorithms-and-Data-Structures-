c7aa7424b5c92dc5b75c4f42f9e7a1ef56ea07a7
adil-10 <103273334+adil-10@users.noreply.github.com>
2022-04-08 23:45:41 +0100

    Add files via upload

---
+++ C# exercises 4/task_4/task_4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] id = {1,
+                2,
+                3,
+                4,
+                5,
+                6,7,8,9,10};
+            string[] stuName = { "Maguire", "Kolling", "Bentley", "Sierra", "Richie", "Brooks", "Knuth", "McConnal", "Gamma", "Weiss" };
+            
+
+            Student[] library = new Student[10];
+
+
+            // create an array of books
+            for (int i = 0; i < library.Length; i++)
+            {
+                library[i] = new Student(id[i], stuName[i]);
+
+            }
+
+
+            // call selection sort on an array of book
+            InsertSortGen(library);
+
+
+            // display array of book after sorting
+
+            for (int i = 0; i < library.Length; i++)
+            {
+                Console.WriteLine(library[i].GetSummary());
+
+            }
+            Console.ReadKey();
+        }
+        static public void InsertSortGen<T>(T[] a) where T : IComparable
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                T value = a[i];
+                int j = i;
+
+                for (; j > 0 && a[j - 1].CompareTo(value) > 0; j--)
+                {
+                    a[j] = a[j - 1];
+                }
+                a[j] = value;
+            }
+        }
+    }
+}
---
+++ C# exercises 4/task_4/task_4/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Student : IComparable
+    {
+
+        private int id;
+        private string stuName;
+
+
+        public Student(int id, string stuName) //constructor with one argument
+        {
+            this.id = id;
+            this.stuName = stuName;
+        }
+
+
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string StuName
+        {
+            get { return stuName; }
+            set { stuName = value; }
+        }
+
+        public int CompareTo(Object obj) //implementation of CompareTo
+        {                   // 		for IComparable
+
+            Student other = (Student)obj;
+            return stuName.CompareTo(other.stuName);
+        }
+        public string GetSummary()
+        {
+            string bookSummary = "ID:" + ID + " " + "Name:" + stuName;
+            return bookSummary;
+
+        }
+    }
+}
